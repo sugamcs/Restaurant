1a78f6908bf92a2d7a5ab8746b424b799c4c1758
sugam <sugambhargav@gmail.com>
2023-11-09 21:13:50 +0530

    Fix more console errors

--- src/index.js
+++ src/index.js
@@ -2,40 +2,30 @@ import { createRestaurantHomepage } from "./home";
 import { createRestaurantMenupage } from "./menu";
 import { createRestaurantAboutpage } from "./about";
 
+document.addEventListener('DOMContentLoaded', function () {
+    createRestaurantHomepage(); // Initial tab
 
-document.addEventListener('DOMContentLoaded', function tabDisplayer() {
-  const btnHome = document.getElementById("home-button");
-  const btnAbout = document.getElementById("about-button");
-  const btnMenu = document.getElementById("menu-button");
-  const contentDiv = document.getElementById("content");
-
-  // Set up the initial page
-  createRestaurantHomepage();
-
-  btnHome.addEventListener("click", function () {
-    clearContent();
-    createRestaurantHomepage();
-  });
-
-  btnAbout.addEventListener("click", function () {
-    clearContent();
-    createRestaurantAboutpage();
-  });
-
-  btnMenu.addEventListener("click", function () {
-    clearContent();
-    createRestaurantMenupage();
-  });
-
-  function clearContent() {
-    // Clear the contentDiv before displaying a new page
-    while (contentDiv.firstChild) {
-      contentDiv.removeChild(contentDiv.firstChild);
-    }
-  }
-}
-
-// Call the function to set up the event listeners
-tabDisplayer();
-
+    const tabs = document.querySelectorAll('button');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function (event) {
+            handleTabClick(event);
+        });
+    });
 });
+
+function handleTabClick(event) {
+    const tabName = event.target.dataset.tab;
+    switch (tabName) {
+        case 'home-button':
+            createRestaurantHomepage();
+            break;
+        case 'menu-button':
+            createRestaurantMenupage();
+            break;
+        case 'about-button':
+            createRestaurantAboutpage();
+            break;
+        default:
+            console.error('Invalid tab name');
+    }
+}
